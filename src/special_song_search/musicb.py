f69091e683552fc39e56d9246c28d7a0c8bf0f40
jmlazaro <juan.lazaro2599@gmail.com>
2023-07-18 02:10:12 -0700

    musicb collecting everything (artist & recording) v1

--- src/special_song_search/musicb.py
+++ src/special_song_search/musicb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 import musicbrainzngs
 import pandas as pd
 from dotenv import load_dotenv
@@ -21,19 +22,21 @@ load_dotenv()
 def main() -> None:
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('--get-artists', dest='get_artists', type=int, default=0)
-    parser.add_argument('--get-recordings', dest='get_recordings', type=int, default=0)
+    parser.add_argument('n_artists', type=int)
+    parser.add_argument('n_recordings', type=int)
     parser.add_argument('-v', dest='verbose', action='store_true', default=False)
     args = parser.parse_args()
 
     connect_to_musicbrainz(verbose=args.verbose)
 
     # Early testing
-    if args.get_artists:
-        artists_us = get_artists_from_country('US', offset=0, artists_limit=args.get_artists, verbose=args.verbose)
+    artists = get_artists_from_country(
+            'US', offset=1000, n_artists=args.n_artists, verbose=args.verbose)
+    globals().update({'artists': artists})
 
-    if args.get_recordings:
-        fr = get_artist_recordings(artist_mbid=artists_us.iloc[1]['id'], verbose=args.verbose)
+    recs = get_artist_recordings(
+            artist_mbid=artists[0][0]['id'], n_recordings=args.n_recordings, verbose=args.verbose)
+    globals().update({'recs': recs})
 
     return
 
@@ -50,30 +53,43 @@ def connect_to_musicbrainz(verbose: bool = False) -> None:
 
     return
 
-def get_artists_from_country(country_code: str, offset: int, artists_limit: int, verbose: bool = False) -> pd.DataFrame:
-    page_limit = artists_limit // SEARCH_BROWSE_LIMIT + min(artists_limit % SEARCH_BROWSE_LIMIT, 1)
+def get_artists_from_country(
+        country_code: str,
+        n_artists: int,
+        offset: int = 0,
+        verbose: bool = False
+    ) -> list[tuple]:
+    n_pages = n_artists // SEARCH_BROWSE_LIMIT + min(n_artists % SEARCH_BROWSE_LIMIT, 1)
 
     artists = []
-    for page_n in range(page_limit):
+    for page_n in range(n_pages):
         if verbose:
             print(f'Getting artists from {country_code}, page {page_n}')
-        page = musicbrainzngs.search_artists(query='', country=country_code)
+        page = musicbrainzngs.search_artists(
+            query='',
+            country=country_code,
+            offset=offset + page_n * SEARCH_BROWSE_LIMIT,
+            limit=SEARCH_BROWSE_LIMIT
+        )
         page_artists = page['artist-list']
         artists += page_artists
 
-    return pd.DataFrame([artist_flattened(artist) for artist in artists])
+    return [artist_flattened(artist) for artist in artists]
 
-def get_artist_info(artist_mbid: str, verbose: bool = False) -> pd.DataFrame:
-    includes = ['tags', 'ratings']
-    artist = musicbrainzngs.get_artist_by_id(id=artist_mbid, includes=includes)
-    artist = artist_flattened(artist)
-    return pd.DataFrame([artist])
+def get_artist_info(artist_mbid: str) -> dict:
+    """ Get detailed artist information given mbid """
+    includes = ['ratings']
 
-def artist_flattened(artist: dict[str, dict]) -> dict[str, str]:
+    return musicbrainzngs.get_artist_by_id(id=artist_mbid, includes=includes)
+
+def artist_flattened(artist: dict) -> tuple[dict, list]:
+    artist_keys_to_keep = {'id', 'type', 'name', 'disambiguation', 'gender',
+                           'country', 'life-span', 'tag-list', 'rating'}
+
     artist_flat = dict()
-    artist_keys_to_keep = ('id', 'type', 'name', 'disambiguation', 'gender', 'country', 'life-span', 'tag-list', 'rating')
-
+    artist_tags = []
     artist_nest = artist['artist'] if 'artist' in artist else artist
+
     for key, value in  artist_nest.items():
         if key not in artist_keys_to_keep:
             continue
@@ -85,50 +101,103 @@ def artist_flattened(artist: dict[str, dict]) -> dict[str, str]:
             if key == 'rating':
                 artist_flat['rating_votes'] = int(value['votes-count'])
                 artist_flat['rating'] = float(value['rating'])
-            if key == 'life-span':
+            elif key == 'life-span':
                 for time in ('begin', 'end'):
                     if time in value:
                         artist_flat[f'life_span_{time}'] = value[time]
 
         elif isinstance(value, list):
             if key == 'tag-list':
-                tags = sorted(value, key=lambda x: (-int(x['count']), x['name']))
-                for n, tag in enumerate(tags):
-                    artist_flat[f'tag_{n}'] = tag['name']
-                    artist_flat[f'tag_{n}_count'] = tag['count']
+                artist_tags = value
 
-    return artist_flat
+    return artist_flat, artist_tags
 
-def get_artist_recordings(artist_mbid: str, verbose: bool = False) -> pd.DataFrame:
+def get_artist_recordings(
+        artist_mbid: str,
+        n_recordings: int = -1,
+        detailed: bool = True,
+        verbose: bool = False
+    ) -> list[tuple]:
+    includes = ['artist-credits', 'tags', 'ratings']
+
+    if n_recordings != -1 and n_recordings < MAX_ARTIST_RECORDINGS:
+        n_pages = n_recordings // SEARCH_BROWSE_LIMIT + min(n_recordings % SEARCH_BROWSE_LIMIT, 1)
+    else:
+        n_pages = MAX_ARTIST_RECORDINGS // SEARCH_BROWSE_LIMIT
+
     recordings = []
-    for page_n in range(MAX_ARTIST_RECORDINGS//SEARCH_BROWSE_LIMIT):
+    for page_n in range(n_pages):
         if verbose:
             print(f'Getting recordings from artist {artist_mbid}, page {page_n}')
-        page = musicbrainzngs.browse_recordings(artist=artist_mbid, limit=SEARCH_BROWSE_LIMIT, offset=page_n * SEARCH_BROWSE_LIMIT)
+        page = musicbrainzngs.browse_recordings(
+            artist=artist_mbid,
+            includes=includes,
+            offset=page_n * SEARCH_BROWSE_LIMIT,
+            limit=SEARCH_BROWSE_LIMIT
+        )
         page_recordings = page['recording-list']
         recordings += page_recordings
 
         if len(page_recordings) < SEARCH_BROWSE_LIMIT:
             break
 
-    return pd.DataFrame(recordings)
+    if detailed:
+        if verbose:
+            print('Getting recording(s) details')
+        mbids = [recording['id'] for recording in recordings]
+        return [recording_flattened(get_recording_info(mbid)) for mbid in mbids]
 
-def get_recording_info(recording_mbid: str, verbose: bool = False) -> pd.DataFrame:
-    includes = ['tags', 'ratings']
-    recording = musicbrainzngs.get_artist_by_id(id=artist_mbid, includes=includes)
-    recording = artist_flattened(recording)
-    return pd.DataFrame([recording])
+    return [recording_flattened(recording) for recording in recordings]
 
-def recording_flattened(recording: dict[str, dict]) -> dict[str, str]:
+def get_recording_info(recording_mbid: str) -> dict:
+    """ Get detailed recording information given mbid """
+    includes = ['releases', 'artist-credits', 'tags', 'ratings']
+    release_type = ['album', 'single', 'ep']
+    release_status = ['official']
+
+    return musicbrainzngs.get_recording_by_id(
+        id=recording_mbid,
+        includes=includes,
+        release_type=release_type,
+        release_status=release_status
+    )
+
+def recording_flattened(recording: dict) -> tuple[dict, list]:
+    recording_keys_to_keep = {'id', 'title', 'length', 'disambiguation',
+                              'release-list', 'tag-list', 'rating',
+                              'artist-credit-phrase'}
+
     recording_flat = dict()
-    recording_keys_to_keep = ('id', 'title', 'length', 'disambiguation', 'tag-list', 'rating')
-
+    recording_tags = []
     recording_nest = recording['recording'] if 'recording' in recording else recording
+
     for key, value in  recording_nest.items():
-        if key not in keys_to_keep:
+        if key not in recording_keys_to_keep:
             continue
 
-    return recording_flat
+        if isinstance(value, str):
+            if key == 'length':
+                recording_flat[key] = int(value)
+            else:
+                recording_flat[key] = value
+
+        elif isinstance(value, dict):
+            if key == 'rating':
+                recording_flat['rating_votes'] = int(value['votes-count'])
+                recording_flat['rating'] = float(value['rating'])
+
+        elif isinstance(value, list):
+            if (key == 'release-list') and value:
+                first_release = min(
+                    value,
+                    key=lambda release: release.get('date', '99999')
+                )
+                recording_flat['date'] = first_release.get('date', None)
+                recording_flat['release_status'] = 'official'
+            elif key == 'tag-list':
+                recording_tags = value
+
+    return recording_flat, recording_tags
 
 
 if __name__ == '__main__':
