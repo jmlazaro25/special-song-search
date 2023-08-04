66818a9d878fef6eb9d1556c9e18dc6ea7abbb6c
jmlazaro <juan.lazaro2599@gmail.com>
2023-08-03 18:04:23 -0700

    add ne recording options to sql statment

--- src/special_song_search/recommend.py
+++ src/special_song_search/recommend.py
@@ -5,6 +5,7 @@
 from sqlalchemy import select
 from sqlalchemy import func
 from sqlalchemy import Float
+from sqlalchemy import Date
 from sqlalchemy.engine.row import RowMapping
 
 from special_song_search.database import init_db
@@ -15,16 +16,26 @@ from special_song_search.models import (
 )
 
 
+# Reused strings
+RECORDING_LENGTH = 'recording_length'
+RECORDING_DATE = 'recording_date'
+CONDITION = 'condition'
+RANGE = 'range'
+CENTER = 'center'
+POINTS_PER_SECOND = 'points_per_second'
+POINTS_PER_YEAR = 'points_per_year'
+
 def recommend(
         session,
         artist_tags: dict[str, float] = dict(),
         recording_tags: dict[str, float] = dict(),
-        recording_length: tuple[int] = tuple(),
-        recording_dates: tuple[str] = tuple(),
         weights: dict[str, float] = dict(),
+        recording_length: dict = dict(),
+        recording_date: dict = dict(),
+        recording_status: str = '',
         randomness: float = 1.,
         random_normal: float = 2.**64,
-        limit: int = 10
+        limit: int = 100
     ) -> list[dict]:
 
     artist_tags_score = 0.0
@@ -35,17 +46,28 @@ def recommend(
     for tag, tag_weight in recording_tags.items():
         recording_tags_score += (RecordingTag.tag == tag) * tag_weight
 
-    recording_length_score = 0.0
-    if len(recording_length) == 1:
-        recording_tags_score += func.abs(Recording.length - recording_length[0])
-
     score  = (
         func.cast(0, Float) # sqlalchemy func needed for label
         + artist_tags_score * weights.get('artist_tags', 0.0)
         + recording_tags_score * weights.get('recording_tags', 0.0)
-        + recording_length_score * weights.get('recording_length', 0.0)
-        #+ recording_dates_score * weights.get('recording_dates', 0.0)
-    ).label('score')
+    )
+
+    if recording_length[f'{RECORDING_LENGTH}_{CONDITION}'] == CENTER:
+        score -= (
+            func.abs(Recording.length / 1_000 - recording_length[CENTER])
+            * recording_length[POINTS_PER_SECOND]
+        )
+
+    if recording_date[f'{RECORDING_DATE}_{CONDITION}'] == CENTER:
+        score -= (
+            func.abs(
+                # Only year extract in cast because some rows only have years
+                func.cast(Recording.date, Date) - recording_date[CENTER]
+            )
+            * recording_date[POINTS_PER_YEAR]
+        )
+
+    score = score.label('score')
 
     filter_score = (
         score
@@ -53,12 +75,34 @@ def recommend(
     ).label('filter_score')
 
     statement = select(Recording, score, filter_score)
+
+    if recording_length[f'{RECORDING_LENGTH}_{CONDITION}'] == RANGE:
+        statement = statement.where(
+            recording_length[RANGE][0] <= Recording.length / 1_000
+        )
+        if recording_length[RANGE][1] is not None:
+            statement = statement.where(
+                Recording.length / 1_000 <= recording_length[RANGE][1]
+            )
+
+    if recording_date[f'{RECORDING_DATE}_{CONDITION}'] == RANGE:
+        statement = statement.where(
+                # Only year extract in cast because some rows only have years
+                recording_date[RANGE][0] <= func.cast(Recording.date, Date)
+        )
+        if recording_date[RANGE][1] is not None:
+            statement = statement.where(
+                 func.cast(Recording.date, Date) <= recording_date[RANGE][1]
+            )
+
     if 'artist_tags' in weights:
-        statement = statement\
+        statement = (statement
             .join(artist_recording_association,
-                Recording.mbid == artist_recording_association.c.recording_mbid)\
-            .join(Artist, artist_recording_association.c.artist_mbid == Artist.mbid)\
+                Recording.mbid == artist_recording_association.c.recording_mbid
+                )
+            .join(Artist, artist_recording_association.c.artist_mbid == Artist.mbid)
             .join(ArtistTag, Artist.mbid == ArtistTag.artist_mbid)
+        )
     if 'recording_tags' in weights:
         statement = statement.join(RecordingTag)
 
@@ -87,4 +131,3 @@ def get_tag_options(session, tag_type: str) -> list[str]:
         table = RecordingTag
     results = session.execute(select(table.tag).distinct()).fetchall()
     return [result[0] for result in results]
-
